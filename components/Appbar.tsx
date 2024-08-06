3d6a7842d938fed3ce0bfef8d679e68e46072c32
Raghavender Singh <92844911+RaghavenderSingh@users.noreply.github.com>
2024-08-06 23:07:47 +0530

    ui update

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -2,21 +2,66 @@
 
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "./ui/button";
+import Link from "next/link";
+import { Menu } from "lucide-react";
 
 export const Appbar = () => {
   const session = useSession();
   return (
-    <div className=" border-b px-2 py-2 flex justify-between">
-      <div className=" text-xl font-bold flex flex-col justify-center">
-        DCEX
+    <header className="py-4 border-b md:border-none fixed top-0 left-0 right-0 z-10 bg-white md:bg-white/0">
+      <div className="container mx-auto px-4 ">
+        <div className="flex justify-between items-center md:border md:p-2.5 rounded-xl max-w-2xl lg:max-w-4xl mx-auto md:bg-white/90 md:backdrop:blur-sm">
+          <div>
+            <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center">
+              {/* <Logo className="h-8 w-8" fill="#000000" /> */}
+            </div>
+          </div>
+          <div className="hidden md:block">
+            <nav className="flex gap-8 text-sm">
+              <Link
+                className="text-black/70 hover:text-black transition"
+                href="#"
+              >
+                Products
+              </Link>
+              <Link
+                className="text-black/70 hover:text-black transition"
+                href="#"
+              >
+                API & Docs
+              </Link>
+              <Link
+                className="text-black/70 hover:text-black transition"
+                href="#"
+              >
+                FAQ
+              </Link>
+              <Link
+                className="text-black/70 hover:text-black transition"
+                href="#"
+              >
+                Company
+              </Link>
+              <Link
+                className="hidden lg:block text-black/70 hover:text-black transition"
+                href="#"
+              >
+                Blogs
+              </Link>
+            </nav>
+          </div>
+          <div className="flex gap-4 items-center">
+            {session.data?.user ? (
+              <Button onClick={() => signOut()}>Logout</Button>
+            ) : (
+              <Button onClick={() => signIn()}>SignIn</Button>
+            )}
+            <span className="md:hidden">
+              <Menu />
+            </span>
+          </div>
+        </div>
       </div>
-      <div>
-        {session.data?.user ? (
-          <Button onClick={() => signOut()}>Logout</Button>
-        ) : (
-          <Button onClick={() => signIn()}>SignIn</Button>
-        )}
-      </div>
-    </div>
+    </header>
   );
 };
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,47 +1,52 @@
-"use client";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { FaGoogle } from "react-icons/fa6";
+import eth from "@/assets/eth.svg";
+import sol from "@/assets/solana.svg";
+import Image from "next/image";
 
-import { signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { Button } from "./ui/button";
-
-export const Hero = () => {
-  const session = useSession();
-  const router = useRouter();
-  console.log(session);
+const Hero = () => {
   return (
-    <div>
-      <div className="text-6xl font-medium">
-        <span>The Indian Cryptocurrency</span>
+    <section className="items-center md:mt-40 mt-28">
+      <div className="container mx-auto px-4">
+        <Image
+          src={sol.src}
+          alt=""
+          className="relative left-[10%] lg:left-[30%]"
+          width={50}
+          height={50}
+        />
+        <Image
+          src={eth.src}
+          alt=""
+          className="absolute md:right-[20%] right-[10%] lg:right-[28%] rotate-12 md:top-[26%] top-[34%] lg:top-[34%]"
+          width={50}
+          height={50}
+        />
+        <div className="max-w-[600px] lg:max-w-[900px] mx-auto">
+          <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-black/70 text-transparent bg-clip-text text-center">
+            Secure Your Crypto with Our Wallet Generator
+          </h1>
 
-        <span className="text-blue-500 pl-4">Revolution</span>
-      </div>
-      <div className="flex justify-center pt-4 text-2xl text-slate-500">
-        Create a frictionless wallet from India with just a Google Account.
-      </div>
-      <div className="flex justify-center pt-2 text-2xl text-slate-500">
-        Convert your INR into Cryptocurrency
-      </div>
-      <div className="pt-8 flex justify-center">
-        {session.data?.user ? (
-          <Button
-            className="bg-blue-500"
-            onClick={() => {
-              router.push("/dashboard");
-            }}
-          >
-            Go to Dashboard
+          <p className="text-lg tracking-tighter text-black/70 text-center mt-5">
+            Generate a secure, private crypto wallet in minutes with our
+            easy-to-use tool <br className="hidden md:block" /> Powered by
+            Google Auth for seamless, secure access.
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-5">
+          <Button className="pl-2 py-6 text-sm md:text-base">
+            <span className="flex items-center gap-2">
+              <div className="px-3 py-2 rounded-lg border bg-white text-black">
+                <FaGoogle />
+              </div>
+              Sign up with Google
+            </span>
           </Button>
-        ) : (
-          <Button
-            className="bg-blue-500"
-            onClick={() => {
-              signIn("google");
-            }}
-          >
-            Login with Google
-          </Button>
-        )}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
+
+export default Hero;
